78e30787805cbec8348d41d001ac3b54045fb78d
irajeshh <85046336+irajeshh@users.noreply.github.com>
2025-05-15 14:36:43 +0530

    Enum extension document added 📔

--- lib/src/List.dart
+++ lib/src/List.dart
@@ -10,7 +10,9 @@ extension ListStringExtention on List<String> {
 }
 
 extension ListOfEnumsExt on List<Enum> {
+  ///Converts the given List of Enums to a List of .text values
   List<String> get toTexts => map((final Enum f) => f.text).toList();
+  ///Converts the given List of Enums to a List of .name values
   List<String> get toNames => map((final Enum f) => f.name).toList();
 }
 
