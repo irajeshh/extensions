6da89c04c2f3b71e6275556385dd79d9cf036c92
irajeshh <85046336+irajeshh@users.noreply.github.com>
2025-12-08 16:06:15 +0530

    Add new typedefs for HeaderEntry and StringIntMap
    Introduces HeaderEntry as a MapEntry<String, String> and StringIntMap as Map<String, int> to improve code clarity and type safety.

--- lib/src/typedefs.dart
+++ lib/src/typedefs.dart
@@ -5,3 +5,7 @@ typedef Json = Map<String, dynamic>;
 
 /// Alias for a JSON object representing HTTP headers, often used for making HTTP requests.
 typedef HeaderJson = Map<String, String>;
+
+typedef HeaderEntry = MapEntry<String, String>;
+
+typedef StringIntMap = Map<String, int>;
